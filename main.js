fec242b549e3bb577c16d6fbe791f01e8b732ced
kulasisin <107702039@mail2.nccu.tw>
2023-08-07 02:45:49 +0800

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import bootstrap from "bootstrap/dist/js/bootstrap";
+// import { Modal } from "bootstrap";
 
-console.log("Hello world!");
+let path = location.pathname;
+let pathArr = path.split("/");
+let pathName = pathArr[pathArr.length - 1];
+let htmlName = pathName.slice(0, pathName.indexOf(".html"));
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  if (htmlName === "index") {
+    const memberModal = new bootstrap.Modal("#member-modal");
+    memberModal.show();
+  }
+});
+
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+// });
+// console.log("Hello world!");
